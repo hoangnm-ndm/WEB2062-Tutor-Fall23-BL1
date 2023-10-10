dbaca6a6d0576f372a37c698ba787ae72683a769
hoangnm.ndm <hoangnm.ndm@gmail.com>
2023-10-10 22:11:05 +0700

    Tutor day 1

---
+++ datas.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: "1", name: "san pham 1", price: 200 },
+  { id: "2", name: "san pham 2", price: 240 },
+  { id: "3", name: "san pham 3", price: 300 },
+];
+
+export default products;
